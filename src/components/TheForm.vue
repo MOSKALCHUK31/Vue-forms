287a9364aac84a5312f48a1970878177d21fc82f
MOSKALCHUK <moskalchuk@MacBook-Air-MOSKALCHUK.local>
2022-07-28 00:07:33 +0300

    numbers / radio / checkboxes / select-option

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,48 +1,49 @@
 <template>
-  <form>
+  <form @submit.prevent="onSubmit">
     <div class="form-control">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" />
+      <input id="user-name" name="user-name" type="text" v-model="name"/>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number" />
+      <input id="age" name="age" type="number" v-model="age" ref="userAge_refs"/>
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
-      <select id="referrer" name="referrer">
+      <select id="referrer" name="referrer" v-model="referrer">
         <option value="google">Google</option>
         <option value="wom">Word of mouth</option>
         <option value="newspaper">Newspaper</option>
       </select>
     </div>
     <div class="form-control">
+      <!-- For checkbox we have to bind data to array, then every checkbox must to have a idnitified value [using value attribute] -->
       <h2>What are you interested in?</h2>
       <div>
-        <input id="interest-news" name="interest" type="checkbox" />
+        <input id="interest-news" name="interest" type="checkbox" value="news" v-model="checkboxes"/>
         <label for="interest-news">News</label>
       </div>
       <div>
-        <input id="interest-tutorials" name="interest" type="checkbox" />
+        <input id="interest-tutorials" name="interest" type="checkbox" value="tutorials" v-model="checkboxes"/>
         <label for="interest-tutorials">Tutorials</label>
       </div>
       <div>
-        <input id="interest-nothing" name="interest" type="checkbox" />
+        <input id="interest-nothing" name="interest" type="checkbox" value="nothing" v-model="checkboxes"/>
         <label for="interest-nothing">Nothing</label>
       </div>
     </div>
     <div class="form-control">
       <h2>How do you learn?</h2>
       <div>
-        <input id="how-video" name="how" type="radio" />
+        <input id="how-video" name="how" type="radio" value="video" v-model="radio"/>
         <label for="how-video">Video Courses</label>
       </div>
       <div>
-        <input id="how-blogs" name="how" type="radio" />
+        <input id="how-blogs" name="how" type="radio" value="blogs" v-model="radio"/>
         <label for="how-blogs">Blogs</label>
       </div>
       <div>
-        <input id="how-other" name="how" type="radio" />
+        <input id="how-other" name="how" type="radio" value="other" v-model="radio"/>
         <label for="how-other">Other</label>
       </div>
     </div>
@@ -52,6 +53,29 @@
   </form>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      age: null, //null because expected type number,
+      referrer: 'google', // set default value (get string from value in option)
+      checkboxes: [], // If we have more than 1 checkbox, we should use array. If checkbox=checked, we will get checked values in array
+      radio: null // null because expect string if one of theese radio will be checked
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log(`Name: ${this.name}`);
+      console.log('Age: ' + this.age); // From v-model we got type number
+      console.log(`Age with refs: ${this.$refs.userAge_refs.value}`); // From refs we got type string [be patient]
+      console.log('Referrer: ' + this.referrer); // Got value from option (Not the same as text)
+      console.log('Checkboxes: ' + this.checkboxes); // We got array values like [val1, val2, val3] if checkbox checked
+      console.log('Radio: ' + this.radio); // We got value attribute from radiobutton input like 'video'
+    }
+  }
+}
+</script>
 <style scoped>
 form {
   margin: 2rem auto;
